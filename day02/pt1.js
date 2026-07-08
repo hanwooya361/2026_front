0cd52b41314d3c2a802653453666fc4a7129fc98
hanwooya361 <johunwoo123456@gmail.com>
2026-07-08 16:55:37 +0900

    day03 7h

--- day02/pt1.js
+++ day02/pt1.js
@@ -1,16 +1,16 @@
-console.log("[주문 접수]");
+console.log("[주문 접수]")
 
-alert("안녕하세요 손님의 성함을 입력해주세요!");
+alert("안녕하세요 손님의 성함을 입력해주세요!")
 console.log("이름")
-let username = prompt('사용자이름: ');
-console.log(username)
+let username = prompt('사용자이름: ')
+console.log(`username: ${username}`)
 
-alert("드시고 싶은 음료 이름을 입력해주세요!");
+alert("드시고 싶은 음료 이름을 입력해주세요!")
 console.log("메뉴")
-let foodname = prompt('메뉴: ');
-console.log(foodname)
+let foodname = prompt('메뉴: ')
+console.log(`foodname: ${foodname}`)
 
-alert("수량을 입력해주세요!");
+alert("수량을 입력해주세요!")
 console.log("수량")
-let quantity = prompt('수량 ');
-console.log(quantity)
+let quantity = Number(prompt('수량 '))
+console.log(`quantity: ${quantity}`)
