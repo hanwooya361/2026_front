822ccc3ad3357742bcf9cb6a4adc04f39c3279f0
hanwooya361 <johunwoo123456@gmail.com>
2026-07-22 12:26:24 +0900

    day12

--- day11/jspt7.js
+++ day11/jspt7.js
@@ -2,13 +2,13 @@ const table = document.querySelector('#table')
 function 등록함수( ){
     const date = document.querySelector('#date').value
     const list = document.querySelector('#list').value
-    const price = Number(document.querySelector('#price').value)
+    const price = document.querySelector('#price').value
     
     table.innerHTML += `
         <tr>
             <td>${date}</td>
             <td>${list}</td>
-            <td>${price.toLocaleString()}</td>
+            <td>${parseInt(price).toLocaleString()}</td>
         </tr>
     `;
 }
